e0d57329578971a831d29d32a607bf425520892a
Dilara Bedir <dilara_bedir@hotmail.com>
2021-02-22 23:27:32 +0300

    Add project files.

---
+++ Gun4Odev5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gun4Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            Console.WriteLine("--------------MyDictionary-------------");
+            myDictionary.Add(1,"Engin Demiroğ");
+            myDictionary.Add(2, "Dilara Bedir");
+            myDictionary.Add(3,"Yazılımcı Geliştirme Kampı");
+            myDictionary.ToList();
+        }
+    }
+}
